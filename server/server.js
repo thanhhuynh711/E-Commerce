97a7c48a4cc71650d7821a1008acc31c76e4307f
thanh711 <thanhsonhoc00@gmail.com>
2023-05-28 18:34:39 +0700

    ff

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 require("dotenv").config();
+const dbConnect = require("./config/dbconnect");
+const initRoutes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 8888;
 app.use(express.json());
 app.use(express.urlencoded({ express: true }));
+dbConnect();
 
-app.use("/", (req, res) => {
-  res.send("SERVER ON");
-});
-
+initRoutes(app);
 app.listen(port, () => {
   console.log("Server funning on the port:" + port);
 });
