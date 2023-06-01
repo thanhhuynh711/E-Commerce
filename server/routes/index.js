1f4887fb06188b6addff028b425d3b70d4dc8841
thanh711 <thanhsonhoc00@gmail.com>
2023-06-01 22:33:33 +0700

    product

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require("./user");
+const productRouter = require("./product");
 const { notFount, errHadler } = require("../middlewares/errHandler");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
+  app.use("/api/product", productRouter);
 
   app.use(notFount);
   app.use(errHadler);
